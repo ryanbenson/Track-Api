faa22b6c45c2e48212372e1a662eeffa2353f48b
Ryan Benson <benson.ryan@gmail.com>
2019-03-04 10:10:00 -0600

    Formats server using prettier

--- server.js
+++ server.js
@@ -1,130 +1,120 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const Task = require('./models/task');
-const Achievement = require('./models/achievement');
-const Link = require('./models/link');
+const Task = require("./models/task");
+const Achievement = require("./models/achievement");
+const Link = require("./models/link");
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/track';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/track";
 
-mongoose.connect(dbUrl, {dbName: 'track', useNewUrlParser: true});
+mongoose.connect(dbUrl, { dbName: "track", useNewUrlParser: true });
 
-app.get('/', async (req, res) => {
-  res.json({status: 'Ok'});
+app.get("/", async (req, res) => {
+  res.json({ status: "Ok" });
 });
 
-app.get('/tasks', async (req, res) => {
+app.get("/tasks", async (req, res) => {
   const tasks = await Task.find({});
   res.json(tasks);
 });
 
-app.get('/achievements', async (req, res) => {
+app.get("/achievements", async (req, res) => {
   const achievements = await Achievement.find({});
   res.json(achievements);
 });
 
-app.get('/links', async (req, res) => {
+app.get("/links", async (req, res) => {
   const links = await Link.find({});
   res.json(links);
 });
 
-app.post('/tasks', async (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
     const newTask = await task.save();
-    return res.json({message: 'Task saved', data: newTask});
-
+    return res.json({ message: "Task saved", data: newTask });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.post('/achievements', async (req, res) => {
+app.post("/achievements", async (req, res) => {
   const achievement = new Achievement(req.body);
   try {
     const newAchievement = await achievement.save();
-    return res.json({message: 'Achievement saved', data: newAchievement});
-
+    return res.json({ message: "Achievement saved", data: newAchievement });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-
-app.post('/links', async (req, res) => {
+app.post("/links", async (req, res) => {
   const link = new Link(req.body);
   try {
     const newLink = await link.save();
-    return res.json({message: 'Link saved', data: newLink});
-
+    return res.json({ message: "Link saved", data: newLink });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.put('/tasks', async (req, res) => {
+app.put("/tasks", async (req, res) => {
   try {
     const updated = await Task.findByIdAndUpdate(req.body._id, req.body);
-    return res.json({message: 'Task updated', data: updated});
-
+    return res.json({ message: "Task updated", data: updated });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.put('/achievements', async (req, res) => {
+app.put("/achievements", async (req, res) => {
   try {
     const updated = await Achievement.findByIdAndUpdate(req.body._id, req.body);
-    return res.json({message: 'Achievement updated', data: updated});
-
+    return res.json({ message: "Achievement updated", data: updated });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.put('/links', async (req, res) => {
+app.put("/links", async (req, res) => {
   try {
     const updated = await Link.findByIdAndUpdate(req.body._id, req.body);
-    return res.json({message: 'Link updated', data: updated});
-
+    return res.json({ message: "Link updated", data: updated });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.delete('/tasks', async (req, res) => {
+app.delete("/tasks", async (req, res) => {
   try {
     const deleted = await Task.findOneAndDelete(req.body._id);
-    return res.json({message: 'Task deleted', data: deleted});
-
+    return res.json({ message: "Task deleted", data: deleted });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.delete('/achievements', async (req, res) => {
+app.delete("/achievements", async (req, res) => {
   try {
     const deleted = await Achievement.findOneAndDelete(req.body._id);
-    return res.json({message: 'Achievement deleted', data: deleted});
-
+    return res.json({ message: "Achievement deleted", data: deleted });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
-app.delete('/links', async (req, res) => {
+app.delete("/links", async (req, res) => {
   try {
     const deleted = await Link.findOneAndDelete(req.body._id);
-    return res.json({message: 'Link deleted', data: deleted});
-
+    return res.json({ message: "Link deleted", data: deleted });
   } catch (err) {
-    return res.status(400).json({message: 'Bad input', error: err});
+    return res.status(400).json({ message: "Bad input", error: err });
   }
 });
 
